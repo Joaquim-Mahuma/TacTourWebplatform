e162394af39be98c756dffbdc7b17d0ae2647810
Joaquim Mahuma <joaquimmahuma14@gmail.com>
2026-04-11 05:26:58 +0100

    .

--- TacTourWebplatform/obj/Debug/net10.0/TacTourWebplatform.AssemblyInfo.cs
+++ TacTourWebplatform/obj/Debug/net10.0/TacTourWebplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TacTourWebplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e749b80e4cef58b2f64f2c12b8949cb0bae90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a58bb2ca88382a847d02c536b75876a4f7c3fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TacTourWebplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TacTourWebplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
